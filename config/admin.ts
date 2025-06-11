7c437f892ee7fb85efe5f85e092fe6618020dfee
Egor Koriakov <36813847+altruistor@users.noreply.github.com>
2025-06-11 23:28:52 +0200

    Update admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,17 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'z8cUcK6tj0UpXTiA6bUIeQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '0J0BJKqH8Rwzzh4jRf3Qqw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'JcM5Y0nw9mLu3OPH8UyUEw=='),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', '6IwS2LWU/veJs1/xkyH4QA=='),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
