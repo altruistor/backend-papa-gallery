fd7ed2c30ae9e4fa9432ec0a1d04795155c1e05e
Egor Koriakov <36813847+altruistor@users.noreply.github.com>
2025-06-11 23:16:53 +0200

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS', ['Qa0zx1Cv', 'Ws9xc2Vb']),
   },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', '0Ab0RMLKw8TyuWq2k1JJvkI31Q0IY6WNkV2PLacrz5c='),
+    },
+  },
 });
