5b0d1922e97aeee4651fc80c5224ff78bb0c3389
Egor Koriakov <36813847+altruistor@users.noreply.github.com>
2025-06-11 23:21:47 +0200

    123

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,7 @@
-export default () => ({
-    
-  });
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', '0Ab0RMLKw8TyuWq2k1JJvkI31Q0IY6WNkV2PLacrz5c='),
+    },
+  },
+});
